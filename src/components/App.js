970be14a52978c069775b1c021284fa98248892d
Trent Davis <trendavisinc@gmail.com>
2019-07-19 01:08:04 -0500

    Complete!

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import ResourceList from './ResourceList';
+import UserList from './UserList';
 
 const App = () => {
     const [resource, setResource] = useState('posts');
         return (
             <div className="ui container" style={{marginTop:50}}>
+                <UserList />
                 <div>
                     <button onClick={() => setResource('posts')} className="ui button primary">Posts</button>
                     <button onClick={() => setResource('todos')} className="ui button primary">ToDos</button>
--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,28 +1,14 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
+import useResources from './useResources';
 
-export class ResourceList extends Component {
-    state = { resources: [] };
-
-    async componentDidMount() {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-
-      this.setState({ resources: response.data })
-    }
-    async componentDidUpdate(prevProps) {
-        if(prevProps.resource !== this.props.resource) {
-            const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-
-            this.setState({ resources: response.data })
-        }
-    }
-    render() {
-        return (
-            <div>
-                {this.state.resources.length}
-            </div>
-        )
-    }
+const ResourceList = ({ resource }) => {
+   
+    const resources = useResources(resource);
+    return (
+        <ul>
+           {resources.map(record => <li key={record.id}>{record.title}</li>)}
+        </ul>
+    )
 }
 
 export default ResourceList
